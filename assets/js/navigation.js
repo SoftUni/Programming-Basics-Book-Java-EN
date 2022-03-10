63def022937bc8d1465e835c46a543724cd30e9e
alexpeev9 <alexpeev9@abv.bg>
2022-03-10 18:26:05 +0200

    Add Image to Header

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,7 +6,7 @@ gitbook.events.bind("page.change", function() {
 			let codeBox = $(link).parent().prev();
 			if (codeBox.is("pre")) {
 				// A code box is found just before the code link --> inject the [Run] button
-				let runButton = $("<a href='https://softuni.org/'>Start a Tech Job!</a>");
+				let runButton = $("<a href='https://softuni.org/'>Start a Tech Job! <img class='logo' src='https://softuni.org/wp-content/uploads/2021/08/SoftUni_NO_Slogan_Horizontal.svg' alt='logo'></a>");
 				codeBox.prepend(runButton);
 				// Delete the original REPL hyperlink from the DOM
 				$(link).parent().remove();
