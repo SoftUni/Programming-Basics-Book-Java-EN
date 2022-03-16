e0deeb38fcc4d0cf27520fefe2574fecd0b2677b
alexpeev9 <alexpeev9@abv.bg>
2022-03-16 14:04:18 +0200

    Update Navigation Responsive Design

--- assets/js/top-menu.js
+++ assets/js/top-menu.js
@@ -2,7 +2,7 @@ gitbook.events.bind("page.change", function() {
     let doc = $('section.markdown-section');
     let topMenu = $(`
         <p class="topnav">
-            <a target="_blank" href="https://softuni.org"><img class="logo" src="/assets/softuni-global-logo.png" alt="logo"></a>
+            <a class="logobox" target="_blank" href="https://softuni.org"><img class="logo" src="/assets/softuni-global-logo.png" alt="logo"></a>
             <a target="_blank" href="https://softuni.org">Learn Programming</a>
             <a target="_blank" href="https://www.youtube.com/c/CodeWithNakov">YouTube Channel</a>
             <a target="_blank" href="https://introprogramming.info">Nakov's Books</a>
